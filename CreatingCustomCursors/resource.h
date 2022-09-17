b24eb025ccb76f5409059b719323cd06d3d9ea2c
Ric DeHoff <ricd@eclectrics.com>
2022-09-17 07:41:28 -0700

    Add project files.

---
+++ CreatingCustomCursors/resource.h
@@ -0,0 +1,18 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ generated include file.
+// Used by Win32Resources.rc
+//
+#define IDC_CURSOR1                     201
+#define IDC_CURSOR2                     202
+#define IDC_CURSOR3                     203
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        204
+#define _APS_NEXT_COMMAND_VALUE         40001
+#define _APS_NEXT_CONTROL_VALUE         1001
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
